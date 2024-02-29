6b651f3447ae1b35bb5df4d11568f1db9bd2ccd8
HenrY <113247707+Henrywhothere@users.noreply.github.com>
2024-02-29 21:30:14 +0700

    Add files via upload

---
+++ (vietnamese) sort_selection.py
@@ -0,0 +1,31 @@
+import random
+
+def san_sinh_mang(n):
+    mang = []
+    for i in range(n):
+        so_ngau_nhien = random.randint(0, 100)
+        mang.append(so_ngau_nhien)
+    return mang
+
+def sap_xep_chon(mang):
+    n = len(mang)
+
+    for i in range(n-1):
+        vi_tri_min = i
+        for j in range(i+1, n):
+            if mang[vi_tri_min] > mang[j]:
+                vi_tri_min = j
+
+        mang[i], mang[vi_tri_min] = mang[i], mang[vi_tri_min]
+        print(i+1, '-', mang)
+
+
+
+def main():
+    mang = san_sinh_mang(10)
+    print("Mang ban dau la: \n",mang)
+    sap_xep_chon(mang)
+    print("Mang sau khi sap xep xong la: \n",mang)
+
+if __name__ == '__main__':
+    main()
